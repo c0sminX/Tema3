81dc81e942d14d0886e5d3e84b9731c18ca5d45a
cosmin <isaccosmin98@gmail.com>
2019-10-31 18:05:09 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valoare,tva;
+            int cota;
+            Console.Write("Introduceti valoarea fara TVA: ");
+            valoare = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Introduceti cota TVAului: ");
+            cota = Convert.ToInt32(Console.ReadLine());
+            tva = (double)cota / 100 * valoare;
+            Console.WriteLine("Valoare TVA: {0}", tva);
+            Console.WriteLine("Valoare factura + TVA: {0}", valoare + tva);
+
+        }
+    }
+}
